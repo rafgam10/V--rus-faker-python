5093a2e2d4b527654f7b6786ef0699160314e1e8
Rafael Timóteo <rafgam19@gmail.com>
2022-03-21 22:32:33 -0300

    first commit

---
+++ vírus.py
@@ -0,0 +1,13 @@
+#Programa que cria um vírus 1.0
+
+print("Crie aqui seu vírus!")
+print("______________________________")
+print()
+x = input("Digite o nome do vírus: ")
+
+def main():
+    q = 0
+    while 1:
+        q = q + 1
+        print("Vírus: ", x, "Cópias: ", q)
+main()
